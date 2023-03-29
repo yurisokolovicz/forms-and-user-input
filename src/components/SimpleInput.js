04064280269b52e85f5f5c98877f2e835ecda9b6
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-29 11:30:06 -0300

    Form Submission with useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,23 +1,22 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 
 const SimpleInput = props => {
-    const [enteredName, setEnteredName] = useState('');
-
-    const nameInputChangedHandler = event => {
-        setEnteredName(event.target.value);
-    };
+    const nameInputRef = useRef();
 
     const formSubmissionHandler = event => {
         event.preventDefault();
 
-        console.log(enteredName);
+        const enteredValue = nameInputRef.current.value;
+        console.log(enteredValue);
+
+        nameInputRef.current.value = ''; // DOM manupulation is bad. Only React should manipulate the DOM.
     };
 
     return (
         <form onSubmit={formSubmissionHandler}>
             <div className="form-control">
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" onChange={nameInputChangedHandler} />
+                <input ref={nameInputRef} type="text" id="name" />
             </div>
             <div className="form-actions">
                 <button>Submit</button>
