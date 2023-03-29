c8c200dcc31783fb069a9a891c6be435f9be0303
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-29 11:08:15 -0300

    Form Submission with useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,29 @@
-const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+import { useState } from 'react';
+
+const SimpleInput = props => {
+    const [enteredName, setEnteredName] = useState('');
+
+    const nameInputChangedHandler = event => {
+        setEnteredName(event.target.value);
+    };
+
+    const formSubmissionHandler = event => {
+        event.preventDefault();
+
+        console.log(enteredName);
+    };
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className="form-control">
+                <label htmlFor="name">Your Name</label>
+                <input type="text" id="name" onChange={nameInputChangedHandler} />
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
