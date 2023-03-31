de54284026135de991ebe441e38134151a789b13
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-31 09:50:20 -0300

    Adding onBlur to the Forms & Input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -15,11 +15,25 @@ const SimpleInput = props => {
 
     const nameInputChangedHandler = event => {
         setEnteredName(event.target.value);
+
+        if (event.target.value.trim() !== '') {
+            setEnteredNameIsValid(true);
+            // We don not need to return here because there is not code after.
+        }
+    };
+
+    const nameInputBlurHandler = event => {
+        setEnteredNameTouched(true);
+
+        if (enteredName.trim() === '') {
+            setEnteredNameIsValid(false);
+        }
     };
 
     const formSubmissionHandler = event => {
         event.preventDefault();
 
+        // setEnteredNameTouched(true) indicates that the user has touched the input field. If the user has not touched the input field, we do not want to show the error message. If the user has touched the input field, we want to show the error message.
         setEnteredNameTouched(true);
 
         // Adding validation - check if it is empty
@@ -45,7 +59,14 @@ const SimpleInput = props => {
         <form onSubmit={formSubmissionHandler}>
             <div className={nameInputClasses}>
                 <label htmlFor="name">Your Name</label>
-                <input ref={nameInputRef} type="text" id="name" onChange={nameInputChangedHandler} value={enteredName} />
+                <input
+                    ref={nameInputRef}
+                    type="text"
+                    id="name"
+                    onChange={nameInputChangedHandler}
+                    onBlur={nameInputBlurHandler}
+                    value={enteredName}
+                />
                 {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
             </div>
             <div className="form-actions">
