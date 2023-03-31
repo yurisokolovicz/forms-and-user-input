a5f633ab631e2b1e529d4028c046e3bc5a163aca
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-31 10:17:08 -0300

    Form Validity using useEffect Hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const SimpleInput = props => {
     const [enteredName, setEnteredName] = useState('');
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+    const [formIsValid, setFormIsValid] = useState(false);
 
     const enteredNameIsValid = enteredName.trim() !== '';
     const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    // If enteredNameIsValid (dependency) changes, useEffect will run again. If enteredNameIsValid does not change, useEffect will not run again.
+    useEffect(() => {
+        if (enteredNameIsValid) {
+            setFormIsValid(true);
+        } else {
+            setFormIsValid(false);
+        }
+    }, [enteredNameIsValid]);
 
     const nameInputChangedHandler = event => {
         setEnteredName(event.target.value);
@@ -42,7 +52,7 @@ const SimpleInput = props => {
                 {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
             </div>
             <div className="form-actions">
-                <button>Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
         </form>
     );
